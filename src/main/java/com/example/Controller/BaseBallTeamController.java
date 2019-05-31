7bb411904b0855a7d13819dd9cc62c6629b74cbd
koichi.nagata <koichi.nagata@rakus.co.jp>
2019-05-31 14:13:30 +0900

    ページ作成完了

--- src/main/java/com/example/Controller/BaseBallTeamController.java
+++ src/main/java/com/example/Controller/BaseBallTeamController.java
@@ -1,5 +1,32 @@
 package com.example.Controller;
 
-public class BaseBallTeamController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.BaseballTeam;
+import com.example.servise.BaseballTeamService;
+
+@Controller
+@RequestMapping("/teamList")
+public class BaseBallTeamController {
+	@Autowired
+	private BaseballTeamService baseBallTeamService;
+
+	@RequestMapping("/")
+	public String showList(Model model) {
+		List<BaseballTeam> teamList = baseBallTeamService.showList();
+		model.addAttribute("teamList",teamList);
+		return "team-list-page";
+	}
+
+	@RequestMapping("/teamDetail")
+	public String showDetail(String teamName, Model model) {
+		BaseballTeam teamDetail = baseBallTeamService.showDetail(teamName);
+		model.addAttribute("teamDetail", teamDetail);
+		return "team-detail-page";
+	}
 }
